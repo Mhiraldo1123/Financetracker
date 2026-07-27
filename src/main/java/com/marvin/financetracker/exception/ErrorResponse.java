153f59632ea395b6ed0ccb0187c0e49b5e34916b
Marvin Hiraldo <Mhiraldo1123@aol.com>
2026-07-27 00:20:40 -0400

    Add GlobalExceptionHandler, ErrorResponse, and application.properties.example

--- src/main/java/com/marvin/financetracker/exception/ErrorResponse.java
+++ src/main/java/com/marvin/financetracker/exception/ErrorResponse.java
@@ -1,4 +1,21 @@
-﻿package com.marvin.financetracker.exception;
+package com.marvin.financetracker.exception;
 
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ErrorResponse {
+    HttpStatus statusCode;
+    LocalDateTime time;
+    String message;
+
+    public ErrorResponse(HttpStatus statusCode, LocalDateTime time, String message){
+        this.statusCode = statusCode;
+        this.time = time;
+        this.message = message;
+    }
+
+
 }
--- src/main/java/com/marvin/financetracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/marvin/financetracker/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
-﻿package com.marvin.financetracker.exception;
+package com.marvin.financetracker.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    //@ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFound(ResourceNotFoundException e){
+        //System.out.println("Error message: " + e.getMessage() + LocalDateTime.now());
+        ErrorResponse e1 = new ErrorResponse(HttpStatus.NOT_FOUND, LocalDateTime.now(), e.getMessage());
+        return new ResponseEntity<ErrorResponse>(e1, e1.getStatusCode());
+
+    }
 }
